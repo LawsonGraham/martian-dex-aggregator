bf3628a4e761d9c3f02688f5a3afc01b38b79412
Lawson Graham <lawson.graham8@gmail.com>
2023-04-15 19:13:59 -0400

    functioning swap backend

--- src/App.js
+++ src/App.js
@@ -1,38 +1,38 @@
-import './App.css';
+import "./App.css";
 import { useState, useEffect } from "react";
-import getCoins from './hooks/getCoins';
-import { scaleUp, parseLiquidityPoolType } from './utils/thalaPools';
+import getCoins from "./hooks/getCoins";
+import { scaleUp, parseLiquidityPoolType } from "./utils/thalaPools";
 
 function App() {
-  const [account, setAccount] = useState("0x7a26be858cbdf707c9b01d9b462a8fadae81cfe28f97805b2d15584208b60436");
+  const [account, setAccount] = useState(
+    "0x7a26be858cbdf707c9b01d9b462a8fadae81cfe28f97805b2d15584208b60436"
+  );
   const [coins, setCoins] = useState([]);
   const [coinsLoading, setCoinsLoading] = useState(true);
   const [swap, setSwap] = useState({});
   const [swapLoading, setSwapLoading] = useState(false);
 
-
   const connectMartian = async () => {
     const response = await window.martian.connect();
-    console.log(response)
-    setAccount(response.address)
+    console.log(response);
+    setAccount(response.address);
   };
 
   const executeSwap = async () => {
     let payload;
-    if (swap.api_type == 'open_ocean') {
+    if (swap.api_type == "open_ocean") {
       //open ocean
       payload = {
         function: swap.data.function,
         type_arguments: swap.data.type_arguments,
         arguments: swap.data.arguments,
       };
-      
     } else {
       // thala
       // TODO: make this work for weighted pools
       const poolType = parseLiquidityPoolType(swap.pool.type);
       let coinInIndex;
-      for (let i = 0; i < swap.pool.coins.length; i++ ) {
+      for (let i = 0; i < swap.pool.coins.length; i++) {
         if (swap.pool.coins[i].address == swap.inputCoin) {
           coinInIndex = i;
         }
@@ -42,13 +42,28 @@ function App() {
       if (poolType == "weighted") {
         const type_args = swap.pool.coinAddresses;
         while (type_args.length != 4) {
-          type_args.push("0x48271d39d0b05bd6efca2278f22277d6fcc375504f9839fd73f74ace240861af::base_pool::Null")
+          type_args.push(
+            "0x48271d39d0b05bd6efca2278f22277d6fcc375504f9839fd73f74ace240861af::base_pool::Null"
+          );
         }
+        
+        const weights = swap.pool.weights;
+        for (let i = 0; i < weights.length; i++) {
+          type_args.push(`0x48271d39d0b05bd6efca2278f22277d6fcc375504f9839fd73f74ace240861af::weighted_pool::Weight_${weights[i]}`)
+        }
+        while (type_args.length != 8) {
+          type_args.push(
+            "0x48271d39d0b05bd6efca2278f22277d6fcc375504f9839fd73f74ace240861af::base_pool::Null"
+            );
+        }
+
+        type_args.push(swap.inputCoin);
+        type_args.push(swap.outputCoin);
 
         payload = {
           type: "entry_function_payload",
           function: `0x48271d39d0b05bd6efca2278f22277d6fcc375504f9839fd73f74ace240861af::weighted_pool_scripts::swap_exact_in`,
-          type_arguments: [],
+          type_arguments: type_args,
           arguments: [
             scaleUp(swap.inputAmount, coinInDecimals).toFixed(0),
             scaleUp(0, coinInDecimals).toFixed(0),
@@ -57,7 +72,9 @@ function App() {
       } else {
         const type_args = swap.pool.coinAddresses;
         while (type_args.length != 4) {
-          type_args.push("0x48271d39d0b05bd6efca2278f22277d6fcc375504f9839fd73f74ace240861af::base_pool::Null")
+          type_args.push(
+            "0x48271d39d0b05bd6efca2278f22277d6fcc375504f9839fd73f74ace240861af::base_pool::Null"
+          );
         }
 
         type_args.push(swap.inputCoin);
@@ -73,10 +90,12 @@ function App() {
           ],
         };
       }
-      
     }
 
-    const transaction = await window.martian.generateTransaction(account, payload);
+    const transaction = await window.martian.generateTransaction(
+      account,
+      payload
+    );
     const txnHash = await window.martian.signAndSubmitTransaction(transaction);
 
     console.log(txnHash);
@@ -85,8 +104,9 @@ function App() {
 
   const quoteThalaSwap = async (inputCoin, outputCoin, amount) => {
     // const res = await fetch(`http://localhost:3000/api/swap?input-coin=0x7fd500c11216f0fe3095d0c4b8aa4d64a4e2e04f83758462f2b127255643615::thl_coin::THL&output-coin=0x1::aptos_coin::AptosCoin&in-or-out-amount=10&pool-limit=3&in-for-out=true`,  {method: 'GET'})
-    const res = await fetch(`http://localhost:3000/api/swap?input-coin=${inputCoin}&output-coin=${outputCoin}&in-or-out-amount=${amount}&pool-limit=3&in-for-out=false`).then( async (res) => 
-    {
+    const res = await fetch(
+      `http://localhost:3000/api/swap?input-coin=${inputCoin}&output-coin=${outputCoin}&in-or-out-amount=${amount}&pool-limit=3&in-for-out=false`
+    ).then(async (res) => {
       const response = await res.json();
 
       if (response) {
@@ -97,36 +117,35 @@ function App() {
         return;
       }
     });
-    console.log('done ThalaQuote')
-    return res
+    console.log("done ThalaQuote");
+    return res;
   };
 
   const quoteOpenOceanSwap = async (inputCoin, outputCoin, amount) => {
     const chain = "aptos";
-    const inTokenAddress = inputCoin.split("::")[0]
-    const outTokenAddress = outputCoin.split("::")[0]
+    const inTokenAddress = inputCoin.split("::")[0];
+    const outTokenAddress = outputCoin.split("::")[0];
     const gasPrice = 5;
     const slippage = 1;
     const res = await fetch(
       `https://open-api.openocean.finance/v3/aptos/swap_quote?inTokenAddress=${inTokenAddress}&outTokenAddress=${outTokenAddress}&amount=${amount}&gasPrice=${gasPrice}&slippage=${slippage}&chain=${chain}&account=${account}`
-    ).then( async (res) => 
-    {
+    ).then(async (res) => {
       const response = await res.json();
       if (response) {
         const { estimatedGas, data, gasPrice } = response;
         // const result = await this.myWallet.sdk.eth.sendTransaction(swapParams);
-        return data
+        return data;
       } else {
         return;
       }
     });
-    console.log('done Quote')
-    return res
+    console.log("done Quote");
+    return res;
   };
 
   const quote = async (inputCoin, outputCoin, amount) => {
-    setSwapLoading(true)
-    console.log('quoting...')
+    setSwapLoading(true);
+    console.log("quoting...");
     let q1 = {};
     let q2 = {};
     try {
@@ -136,48 +155,54 @@ function App() {
       q2 = await quoteThalaSwap(inputCoin, outputCoin, amount);
     } catch {}
 
-    if (q1 != undefined) { // if the token isn't supported by open ocean
-    q1.outAmount = parseFloat(q1.outAmount) / 100000000 - 12
-    q1.api_type = 'open_ocean'
+    if (q1 != undefined) {
+      // if the token isn't supported by open ocean
+      q1.outAmount = parseFloat(q1.outAmount) / 100000000;
+      q1.api_type = "open_ocean";
     }
 
     if (q2 != undefined) {
-      q2.api_type = 'thala'
-      q2.outAmount = q2.inputAmount
+      q2.api_type = "thala";
+      q2.outAmount = q2.inputAmount;
     }
 
-    console.log(q1)
-    console.log(q2)
+    console.log(q1);
+    console.log(q2);
 
     if (q1 && q2 && q1.outAmount > q2.outAmount) {
-      setSwap(q1)
+      setSwap(q1);
     } else if (!q1 && q2) {
-      setSwap(q2)
+      setSwap(q2);
     } else {
-      setSwap(q1)
+      setSwap(q1);
     }
-    setSwapLoading(false)
-  }
+    setSwapLoading(false);
+  };
 
   useEffect(() => {
     if (coins.length == 0) {
-      getCoins().then(res => {
+      getCoins().then((res) => {
         setCoins(res);
-        setCoinsLoading(false)
+        setCoinsLoading(false);
       });
     }
   });
-  
-  
-
 
   return (
-
     <div className="flex min-h-screen flex-col items-center justify-between p-24">
       <button onClick={executeSwap} className="large">
         makeSwap
       </button>
-      <button onClick={() => quote("0xa2eda21a58856fda86451436513b867c97eecb4ba099da5775520e0f7492e852::coin::T", "0x1::aptos_coin::AptosCoin", 0.5)} className="large">
+      <button
+        onClick={() =>
+          quote(
+            "0x7fd500c11216f0fe3095d0c4b8aa4d64a4e2e04f83758462f2b127255643615::thl_coin::THL",
+            "0x1::aptos_coin::AptosCoin",
+            0.5
+          )
+        }
+        className="large"
+      >
         quote swaps
       </button>
       <button onClick={quoteThalaSwap} className="large">
@@ -186,16 +211,16 @@ function App() {
       <button onClick={connectMartian} className="large">
         connectMartian
       </button>
-      {!coinsLoading && coins.map(coin => {
-        return(
-        <div>
-          coin: {coin.name}
-        </div>
-        )
-      })}
-      {swapLoading ? <div>loading</div> : <div>swap type chosen: {swap.api_type}</div> }
+      {!coinsLoading &&
+        coins.map((coin) => {
+          return <div>coin: {coin.name}</div>;
+        })}
+      {swapLoading ? (
+        <div>loading</div>
+      ) : (
+        <div>swap type chosen: {swap.api_type}</div>
+      )}
     </div>
-
   );
 }
 
--- src/utils/thalaPools.js
+++ src/utils/thalaPools.js
@@ -2,22 +2,19 @@ import BigNumber from "bignumber.js";
 
 export function scaleUp(v, decimals) {
   return new BigNumber(v)
-    .multipliedBy((new BigNumber(10)).exponentiatedBy(decimals))
+    .multipliedBy(new BigNumber(10).exponentiatedBy(decimals))
     .toNumber();
 }
 
 // Returns [liquidityPoolType, coinAddresses]
-export function parseLiquidityPoolType(
-  poolType,
-) {
+export function parseLiquidityPoolType(poolType) {
   const matchWeightedPool = poolType.includes("weighted_pool::WeightedPool<");
   if (matchWeightedPool) {
-    return "weighted"
+    return "weighted";
   }
   const matchStablePool = poolType.includes("stable_pool::StablePool<");
   if (matchStablePool) {
-    return "stable"
+    return "stable";
   }
   throw new Error(`Invalid poolType: ${poolType}`);
 }
-
